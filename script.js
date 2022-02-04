9559c961f2de51a5caef984216a3f0d0878923ab
Abdulboriy8286 <israilovabdulboriy8286@gmail.com>
2022-02-04 18:07:04 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+const form = document.querySelector('.form');
+const containerWorkouts = document.querySelector('.workouts');
+const inputType = document.querySelector('.form__input--type');
+const inputDistance = document.querySelector('.form__input--distance');
+const inputDuration = document.querySelector('.form__input--duration');
+const inputCadence = document.querySelector('.form__input--cadence');
+const inputElevation = document.querySelector('.form__input--elevation');
+
+if(navigator.geolocation){
+    navigator.geolocation.getCurrentPosition(function(position){
+        const {latitude, longitude, accuracy} = position.coords
+        console.log(accuracy)
+
+        const map = L.map('map').setView([latitude, longitude], 20);
+
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
+    
+        L.marker([latitude, longitude]).addTo(map)
+            .bindPopup('I am here')
+            .openPopup();
+
+        map.on("click", function(event){
+            const {lat , lng} = event.latlng
+
+            L.marker([lat, lng])
+            .addTo(map)
+            .bindPopup(
+                L.popup({
+                    maxWidth: 250,
+                    minWidth: 150,
+                    autoClose: false,
+                    closeOnClick: false,
+                    className: "running-popup"
+                })
+            ).setPopupContent("MARKER")
+            .openPopup();
+        })
+    })
+}
